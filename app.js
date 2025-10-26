86d6ca916d4e8e1b465ab8a8e4c12dfb0b91c67e
Kibe Kevin <kibekevin215@gmail.com>
2025-10-26 16:11:37 +0300

    Add Error Handling Middleware

--- app.js
+++ app.js
@@ -5,20 +5,30 @@ import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import listingRouter from './routes/listing.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+//Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
+//Routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/listings', listingRouter)
 
+//Error middleware
+app.use(errorMiddleware);
 
+//Test route
 app.get('/', (req, res) => {
     res.send('Hello, PeakNest API!');
 })
 
-
+//Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 
