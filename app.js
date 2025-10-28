f2679df510bd14c75c064a1393d16526ee7f462d
Kibe Kevin <kibekevin215@gmail.com>
2025-10-28 17:13:28 +0300

    Add Rate Limiting functionality using Arcjet

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import listingRouter from './routes/listing.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import ArcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -14,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(ArcjetMiddleware);
 
 //Routes
 app.use('/api/v1/auth', authRouter)
